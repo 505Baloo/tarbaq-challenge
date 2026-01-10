65aebe328a229416461808edd4679fd1dc740ffd
Jordan <jordan.cheniclet@gmail.com>
2026-01-10 17:03:23 +0100

    :rocket: fix vite config to be able to publish to github page

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/tarbaq-challenge">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
